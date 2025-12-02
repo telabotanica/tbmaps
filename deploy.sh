64da9f55cd8a50251ea76416b369041dfb4681fe
Julien MARTIN <ju.martin@outlook.com>
2025-12-02 09:00:23 +0100

    npm update, add beta environment, add deploy script

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Configuration
+
+# Arguments
+CONFIG=$1
+
+if [ "$CONFIG" == "production" ]; then
+  BUILD_DIR="dist/tbmaps-angular/production/browser"
+  TARGET_PATH="/home/telaorg/www/identiplante"
+  SERVER="telaorg@sycomore"
+elif [ "$CONFIG" == "beta" ]; then
+  BUILD_DIR="dist/tbmaps-angular/beta/browser"
+  TARGET_PATH="/home/beta/www/identiplante"
+  SERVER="beta@aphyllanthe"
+else
+  echo "Invalid configuration. Use 'production' or 'beta'."
+  exit 1
+fi
+
+# Build and deploy
+echo "Building with configuration: $CONFIG..."
+ng build --configuration $CONFIG --base-href=/tbmaps/
+
+echo "Deploying to server: $SERVER, path: $TARGET_PATH..."
+rsync -r $BUILD_DIR/* $SERVER:$TARGET_PATH
+
+echo "Deployment completed."
