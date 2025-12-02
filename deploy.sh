422b7f7b5dd76374bbf28041f0e0bd06846c6980
Julien MARTIN <ju.martin@outlook.com>
2025-12-02 09:14:03 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ CONFIG=$1
 
 if [ "$CONFIG" == "production" ]; then
   BUILD_DIR="dist/tbmaps-angular/production/browser"
-  TARGET_PATH="/home/telaorg/www/identiplante"
+  TARGET_PATH="/home/telaorg/www/tbmaps"
   SERVER="telaorg@sycomore"
 elif [ "$CONFIG" == "beta" ]; then
   BUILD_DIR="dist/tbmaps-angular/beta/browser"
-  TARGET_PATH="/home/beta/www/identiplante"
+  TARGET_PATH="/home/beta/www/tbmaps"
   SERVER="beta@aphyllanthe"
 else
   echo "Invalid configuration. Use 'production' or 'beta'."
