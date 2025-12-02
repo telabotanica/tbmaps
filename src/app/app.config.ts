4ff3d85c3b33eee4c2e8f4dfedee8c11cd551314
Julien MARTIN <ju.martin@outlook.com>
2025-12-02 09:27:40 +0100

    update Angular from 17 to 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,7 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import {HttpClientModule, provideHttpClient} from "@angular/common/http";
+import { provideHttpClient} from "@angular/common/http";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideAnimations} from "@angular/platform-browser/animations";
 
@@ -10,7 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(HttpClientModule),
     provideAnimationsAsync(),
     provideAnimations()
   ]
